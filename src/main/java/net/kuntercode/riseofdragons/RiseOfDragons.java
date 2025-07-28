30e57c8971c103e5a2186346a1ba47494e0b23bc
Pinchas Stern <pinhastern@gmail.com>
2025-07-28 16:11:40 +0300

    Added custom creative mode tabs

--- src/main/java/net/kuntercode/riseofdragons/RiseOfDragons.java
+++ src/main/java/net/kuntercode/riseofdragons/RiseOfDragons.java
@@ -1,6 +1,7 @@
 package net.kuntercode.riseofdragons;
 
 import net.kuntercode.riseofdragons.block.ModBlocks;
+import net.kuntercode.riseofdragons.item.ModCreativeModeTabs;
 import net.kuntercode.riseofdragons.item.ModItems;
 import net.minecraft.world.item.CreativeModeTabs;
 import org.slf4j.Logger;
@@ -38,6 +39,8 @@ public class RiseOfDragons {
         // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
+
+        ModCreativeModeTabs.register(modEventBus);
 
         ModItems.register(modEventBus);
         ModBlocks.register(modEventBus);
---
+++ src/main/java/net/kuntercode/riseofdragons/item/ModCreativeModeTabs.java
@@ -0,0 +1,52 @@
+package net.kuntercode.riseofdragons.item;
+
+import net.kuntercode.riseofdragons.RiseOfDragons;
+import net.kuntercode.riseofdragons.block.ModBlocks;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+import java.util.function.Supplier;
+
+public class ModCreativeModeTabs {
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TAB =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, RiseOfDragons.MOD_ID);
+
+    public static final Supplier<CreativeModeTab> VENGESTONE_ITEMS_TAB = CREATIVE_MODE_TAB.register("vengestone_items_tab",
+            () -> CreativeModeTab.builder()
+                    .icon(() -> new ItemStack(ModItems.VENGESTONE_INGOT.get()))
+                    .title(Component.translatable("creativetab.riseofdragons.vengestone_items"))
+                    .displayItems((itemDisplayParameters, output) -> {
+                        output.accept(ModItems.RAW_VENGESTONE);
+                        output.accept(ModItems.VENGESTONE_INGOT);
+                        output.accept(ModItems.VENGESTONE_NUGGET);
+
+                        output.accept(ModBlocks.VENGESTONE_BLOCK);
+                        output.accept(ModBlocks.VENGESTONE_ORE);
+                        output.accept(ModBlocks.DEEPSLATE_VENGESTONE_ORE);
+
+                    })
+
+                    .build());
+
+    public static final Supplier<CreativeModeTab> WEAPONS_ITEMS_TAB = CREATIVE_MODE_TAB.register("weapons_items_tab",
+            () -> CreativeModeTab.builder()
+                    .withTabsBefore(ResourceLocation.fromNamespaceAndPath(RiseOfDragons.MOD_ID, "vengestone_items_tab"))
+                    .icon(() -> new ItemStack(ModBlocks.VENGESTONE_BLOCK.get()))
+                    .title(Component.translatable("creativetab.riseofdragons.weapons_items"))
+                    .displayItems((itemDisplayParameters, output) -> {
+                        output.accept(ModItems.VENGESTONE_INGOT);
+
+                    })
+
+                    .build());
+
+
+    public static void register(IEventBus eventBus) {
+        CREATIVE_MODE_TAB.register(eventBus);
+    }
+}
